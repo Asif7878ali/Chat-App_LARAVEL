9c3942c5d2cb4efbda4ee03ed4c6dc935a3442f6
AsifRiseofPower <aloasif31@gmail.com>
2024-10-19 23:18:18 +0530

    working in migration and model

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable =[
+        'user_id_1',
+        'user_id_2',
+        'last_message_id'
+    ];
+
+    public function lastMessage(){
+        return $this->belongsTo(Message::class, 'last_message_id');
+    }
+
+    public function user_1(){
+        return $this->belongsTo(User::class, 'user_id_1');
+    }
+
+    public function user_2(){
+        return $this->belongsTo(User::class, 'user_id_2');
+    }
 }
